5a1fb987ff86f1156b54c01789ad33639b1c7ecb
MasterBlck <add_090@hotmail.com>
2016-07-11 14:20:24 -0500

    agregamos la actividad menu tabular.java

---
+++ app/src/main/java/com/example/ortega/vaccinesapp/MenuTabular.java
@@ -0,0 +1,13 @@
+package com.example.ortega.vaccinesapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MenuTabular extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_tabular);
+    }
+}
