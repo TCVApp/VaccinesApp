6e3fe3559dd59dcfa07bd8fd54dde2e8b6c05c60
MasterBlck <add_090@hotmail.com>
2016-07-12 13:00:07 -0500

    Probamos los tabs ya corriendo, y modificamos el AndroidManifest para que ejecute la actividad del MenuTabular

--- app/src/main/java/com/example/ortega/vaccinesapp/MenuTabular.java
+++ app/src/main/java/com/example/ortega/vaccinesapp/MenuTabular.java
@@ -1,13 +1,44 @@
 package com.example.ortega.vaccinesapp;
 
+import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TabHost;
 
 public class MenuTabular extends AppCompatActivity {
+    TabHost tabs;
+    Resources recursos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_tabular);
+
+        recursos = super.getResources();
+
+        tabs = (TabHost) super.findViewById(android.R.id.tabhost);
+        tabs.setup();
+
+        TabHost.TabSpec spec = tabs.newTabSpec("miTab1");
+        spec.setContent(R.id.tab1);
+        spec.setIndicator("TAB1", recursos.getDrawable(android.R.drawable.ic_btn_speak_now));
+        tabs.addTab(spec);
+
+
+        spec = tabs.newTabSpec("miTab2");
+        spec.setContent(R.id.tab2);
+        spec.setIndicator("TAB2", recursos.getDrawable(android.R.drawable.ic_dialog_map));
+        tabs.addTab(spec);
+
+        tabs.setCurrentTab(0);
+
+        //Implementacion del evento para visualizar las tabulaciones
+        tabs.setOnTabChangedListener(new TabHost.OnTabChangeListener() {
+            @Override
+            public void onTabChanged(String tabId) {
+                Log.i("Android Tabs", "Pestaña pulsada: " +tabId);
+            }
+        });
     }
 }
