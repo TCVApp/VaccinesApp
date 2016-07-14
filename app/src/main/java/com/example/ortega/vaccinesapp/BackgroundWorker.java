992c487524cd1efff7bbf3c48a33b6d220d79ae0
MasterBlck <198add@gmail.com>
2016-07-13 21:25:17 -0500

    Implementacion del login en modo basico

---
+++ app/src/main/java/com/example/ortega/vaccinesapp/BackgroundWorker.java
@@ -0,0 +1,105 @@
+package com.example.ortega.vaccinesapp;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.os.AsyncTask;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
+
+/**
+ * Created by Ortega on 13/07/2016.
+ */
+public class BackgroundWorker  extends AsyncTask<String, Void, String>{
+    Context contexto;
+    AlertDialog dialogoAlerta;
+
+    public BackgroundWorker(Context ctx){
+        this.contexto = ctx;
+    }
+    @Override
+    protected String doInBackground(String... parametros) {
+        String tipo = parametros[0];
+        String login_url = "http://192.168.10.34/Fepro/Login.php";
+
+        if (tipo.equals("Login")){
+            try {
+                //regogemos los parametros que vamos a procesar
+                String nAfiliacion = parametros[1];
+                String password = parametros[2];
+                URL url = new URL(login_url);
+
+                //Abrir conexion con el servidor por medio de http
+                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+                httpURLConnection.setRequestMethod("POST");                                        //El método de POST lo recibira el script.php
+                httpURLConnection.setDoOutput(true);
+                httpURLConnection.setDoInput(true);
+
+                //Flujo de salida
+                OutputStream flujoSalida = httpURLConnection.getOutputStream();
+
+                //Escritura en el buffer
+                BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(flujoSalida, "UTF-8"));
+
+                //Código pesado para extaer los datos de la peticion
+                String post_data = URLEncoder.encode("afiliacion", "UTF-8") +"="+ URLEncoder.encode(nAfiliacion, "UTF-8") +"&"    //Para el _POST["afiliacion"]
+                        +URLEncoder.encode("password", "UTF-8") +"="+ URLEncoder.encode(password, "UTF-8");                       //Para el _POST["password"]
+                bufferedWriter.write(post_data);
+                Log.e("Valor de POST", post_data);
+                bufferedWriter.flush();
+                bufferedWriter.close();
+
+                //Entrada de datos desde la conexion htto
+                InputStream flujoEntrada = httpURLConnection.getInputStream();
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(flujoEntrada, "iso-8859-1"));
+                String resultado = "";
+                String linea = "";
+
+                while ((linea = bufferedReader.readLine()) != null){
+                    resultado += linea;
+                }
+
+                bufferedReader.close();
+                flujoEntrada.close();
+                httpURLConnection.disconnect();
+                Log.e("Valor de res", resultado);
+
+                return resultado;
+
+            }catch (MalformedURLException e){
+                e.printStackTrace();
+            }catch (IOException e){
+                e.printStackTrace();
+            }
+        }
+
+        return null;
+    }
+
+    @Override
+    protected void onPreExecute(){
+        dialogoAlerta = new AlertDialog.Builder(contexto).create();
+        dialogoAlerta.setTitle("Login Status");
+    }
+
+    @Override
+    protected void onPostExecute(String resultado){
+        dialogoAlerta.setMessage(resultado);
+        dialogoAlerta.show();
+    }
+
+    @Override
+    protected void onProgressUpdate(Void... values) {
+        super.onProgressUpdate(values);
+    }
+}
--- app/src/main/java/com/example/ortega/vaccinesapp/LoginActivity.java
+++ app/src/main/java/com/example/ortega/vaccinesapp/LoginActivity.java
@@ -1,52 +1,35 @@
 package com.example.ortega.vaccinesapp;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
-    Button btnLogin;
+
     EditText etAfiliacion, etPassword;
-    Paciente paciente;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+
         iniciarComponentes();
-
-        btnLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                paciente = new Paciente(etAfiliacion.getText().toString(), etPassword.getText().toString());
-                Toast toast;
-
-                //Primero validamos usuario
-                if(!etAfiliacion.getText().toString().equals(paciente.obtenerAfiliacion())){  //si el nombre de usuario NO es correcto o NO se encuentra en la base de datos
-                    //Se lanza un cuadro de dialogo que indica al usuario que el No. de afiliacion no es correcto
-
-                    toast = Toast.makeText(getApplicationContext(), "El paciente no está registrado ", Toast.LENGTH_SHORT);
-                    toast.show();
-                }else if(!etPassword.getText().toString().equals(paciente.obtenerPassword())){  // si la contraseña NO es correcta
-                    toast = Toast.makeText(getApplicationContext(), "Contraseña Incorrecta", Toast.LENGTH_LONG);
-                    toast.show();
-                }else {  //si el usuario y contraseña son correctos, entonces loguea a la aplicacion
-                    Intent intent = new Intent(LoginActivity.this, InicioActivity.class);
-                    startActivity(intent);
-                }
-            }
-        });
-
     }
 
     private void iniciarComponentes(){
-        btnLogin = (Button) super.findViewById(R.id.buttonLogin);
         etAfiliacion = (EditText) super.findViewById(R.id.etNumAfiliacion);
         etPassword = (EditText) super.findViewById(R.id.etPassword);
+    }
+
+    public void onLoguear(View view){
+        String nAfiliacion = etAfiliacion.getText().toString();
+        String pass = etPassword.getText().toString();
+
+        //Creamos el objeto BackgroundWorker y pasamos como parametro el contexto de ésta clase para que el procesamiento lo haga en segundo plano
+        BackgroundWorker backgroundWorker = new BackgroundWorker(this);
+        backgroundWorker.execute("Login", nAfiliacion, pass);   //Pasamos los argumentos con los que se debe de trabajar en segundo plano
+
     }
 }
